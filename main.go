14d78ebd0dd0ba860dbb71915c055eabdc2fc213
alstn2468 <alstn2468_@naver.com>
2021-02-14 21:24:49 +0900

    #2 Add const, variable declaration example

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	const constName string = "minsu" // 상수 with 타입 선언
+	var variableNameOne string = "minsu" // 변수 with 타입 선언
+	variableNameTwo := "minsu" // 축약된 변수 선언 with 타입 추론 only 함수 안에서만
+
+	fmt.Println(constName)
+	fmt.Println(variableNameOne)
+	fmt.Println(variableNameTwo)
 }
