f5cf8b93406d456b7d6261c34d6c7ea25b0c14ee
alstn2468 <alstn2468_@naver.com>
2021-02-18 21:08:42 +0900

    #3 Add functions part one code

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
 
 func main() {
-	const constName string = "minsu" // 상수 with 타입 선언
-	var variableNameOne string = "minsu" // 변수 with 타입 선언
-	variableNameTwo := "minsu" // 축약된 변수 선언 with 타입 추론 only 함수 안에서만
-
-	fmt.Println(constName)
-	fmt.Println(variableNameOne)
-	fmt.Println(variableNameTwo)
+	totalLenght, _ := lenAndUpper("nico")
+	fmt.Println(totalLenght)
 }
